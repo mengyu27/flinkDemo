aac5d2b5eead85957963ee996bbc9223a6669c73
liuml <liuml@yuyancloud.com.cn~>
2023-04-03 17:22:09 +0800

    feat:提交TransformTest

---
+++ src/main/java/com/lml/transform/TransformTest.java
@@ -0,0 +1,40 @@
+package com.lml.transform;
+
+import cn.hutool.core.util.StrUtil;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+import java.util.List;
+
+/**
+ * 一些简单常用的方法
+ * @author LML
+ * @date 2023/4/3
+ **/
+public class TransformTest {
+    public static void main(String[] args) throws Exception {
+        //创建运行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<String> dataStream = env.readTextFile("E:\\peoject\\flinkDemo\\src\\main\\resources\\SourceCollectionFileTest.txt");
+        DataStream<Integer> dataLength = dataStream.map(new MapFunction<String,Integer>() {
+            @Override
+            public Integer map(String o) throws Exception {
+                return o.length();
+            }
+        });
+        DataStream<String> flatMapStream = dataStream.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String s, Collector<String> collector) throws Exception {
+                List<String> split = StrUtil.split(s, StrUtil.COMMA);
+                split.forEach(d -> collector.collect(d));
+            }
+        });
+        dataLength.print();
+        flatMapStream.print();
+        env.execute();
+    }
+}
