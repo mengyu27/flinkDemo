d9a26d0033a83bf6089d97eb2233d9c7efce994d
liuml <liuml@yuyancloud.com.cn~>
2023-06-06 11:12:22 +0800

    feat:提交TransformSplitStream

---
+++ src/main/java/com/lml/transform/TransformSplitStream.java
@@ -0,0 +1,23 @@
+package com.lml.transform;
+
+import cn.hutool.json.JSONUtil;
+import com.lml.apitest.pojo.SensorReading;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * 分解流
+ * @author LML
+ * @date 2023/5/5
+ **/
+public class TransformSplitStream {
+    public static void main(String[] args) {
+        //创建运行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<String> dataStream = env.readTextFile("E:\\peoject\\flinkDemo\\src\\main\\resources\\SourceCollectionFileTest.txt");
+        //转换为对象流
+        DataStream<SensorReading> dataStreamMap = dataStream.map(line -> JSONUtil.toBean(line, SensorReading.class));
+//        dataStreamMap.sinkTo()
+    }
+}
